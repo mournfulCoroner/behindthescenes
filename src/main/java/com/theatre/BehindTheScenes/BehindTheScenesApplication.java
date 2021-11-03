af0e463876094dbe39783a86d95d5b9daff8ecd4
coroner <linayn9@gmail.com>
2021-11-03 23:47:09 +0300

    Initializing project

---
+++ src/main/java/com/theatre/BehindTheScenes/BehindTheScenesApplication.java
@@ -0,0 +1,13 @@
+package com.theatre.BehindTheScenes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BehindTheScenesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BehindTheScenesApplication.class, args);
+	}
+
+}
