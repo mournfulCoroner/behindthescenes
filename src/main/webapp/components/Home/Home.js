544abde5f9a5ec2ce8a09a61b9268d7cef1501b8
mournful <linayn9@gmail.com>
2021-11-21 03:09:34 +0300

    Pictures on Home

--- src/main/webapp/components/Home/Home.js
+++ src/main/webapp/components/Home/Home.js
@@ -1,6 +1,9 @@
 import React from "react";
-import { withRouter } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import "./Home.css";
+import books from "../../resources/imgs/old-books.jpg";
+import people from "../../resources/imgs/people.jpg";
+import numbers from "../../resources/imgs/numbers.jpg";
 
 const Home = (props) => {
 
@@ -18,46 +21,47 @@ const Home = (props) => {
                 <div className="container">
                     <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
                         <div className="col">
-                            <div className="card shadow-sm">
-                                <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="photo" preserveAspectRatio="xMidYMid slice" focusable="false">
-                                    <title>Placeholder</title>
-                                    <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">
-                                        </text></svg>
-                                <div className="card-body">
-                                    <p className="card-text text-dark">Сценарии</p>
-                                    <div className="d-flex justify-content-between align-items-center">
-                                        <small className="text-muted me-ms-auto">Сценарии, записанные в базу театра</small>
+                            <Link to="/scripts" className="text-decoration-none">
+                                <div className="card shadow-sm">
+                                    <img className="bd-placeholder-img card-img-top" width="100%" height="225" src={books} role="img" aria-label="photo" preserveAspectRatio="xMidYMid slice" focusable="false">
+                                    </img>
+                                    <div className="card-body">
+                                        <p className="card-text text-dark">Сценарии</p>
+                                        <div className="d-flex justify-content-between align-items-center">
+                                            <small className="text-muted me-ms-auto">Сценарии, записанные в базу театра</small>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
+                            </Link>
                         </div>
                         <div className="col">
-                        <div className="card shadow-sm">
-                                <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="photo" preserveAspectRatio="xMidYMid slice" focusable="false">
-                                    <title>Placeholder</title>
-                                    <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">
-                                        </text></svg>
-                                <div className="card-body">
-                                    <p className="card-text text-dark">Актёры</p>
-                                    <div className="d-flex justify-content-between align-items-center">
-                                        <small className="text-muted me-ms-auto">Весь штат театра по клику!</small>
+                            <Link to="/actors" className="text-decoration-none">
+                                <div className="card shadow-sm">
+                                    <div className="img-handler">
+                                        <img className="bd-placeholder-img card-img-top" width="100%" src={people} role="img" aria-label="photo" preserveAspectRatio="xMidYMid slice" focusable="false">
+                                        </img></div>
+                                    <div className="card-body">
+                                        <p className="card-text text-dark">Актёры</p>
+                                        <div className="d-flex justify-content-between align-items-center">
+                                            <small className="text-muted me-ms-auto">Весь штат театра по клику!</small>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
+                            </Link>
                         </div>
                         <div className="col">
-                        <div className="card shadow-sm">
-                                <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="photo" preserveAspectRatio="xMidYMid slice" focusable="false">
-                                    <title>Placeholder</title>
-                                    <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">
-                                        </text></svg>
-                                <div className="card-body">
-                                    <p className="card-text text-dark">Расписание</p>
-                                    <div className="d-flex justify-content-between align-items-center">
-                                        <small className="text-muted me-ms-auto">Выступления в формате календаря</small>
+                            <Link to="/plays/now" className="text-decoration-none">
+                                <div className="card shadow-sm">
+                                    <img className="bd-placeholder-img card-img-top" width="100%" height="225" src={numbers} role="img" aria-label="photo" preserveAspectRatio="xMidYMid slice" focusable="false">
+                                    </img>
+                                    <div className="card-body">
+                                        <p className="card-text text-dark">Расписание</p>
+                                        <div className="d-flex justify-content-between align-items-center">
+                                            <small className="text-muted me-ms-auto">Выступления в формате календаря</small>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
+                            </Link>
                         </div>
                     </div>
                 </div>
